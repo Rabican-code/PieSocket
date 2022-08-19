e23b3c9d6f0d3661e6b5dd535009c538eccdd628
Rabican-code <56071999+Rabican-code@users.noreply.github.com>
2022-08-19 11:10:25 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,281 +1,92 @@
-.footer {
-    background-color: #1f2937;
-    color: #000000;
-    font-size: 16px;
-    font-weight: 400;
-    height: 397px;
-    line-height: 24px;
-    width: 1741px;
-    position: absolute;
-    top: 3500px;
-    left: -30px;
-    z-index: -1;
+import { Link } from "react-router-dom";
+import "./Footer.css"
+import {Resources,Support,Company,Legal} from "./FooterData"
+function Footer() {
+
+    return (
+        <>
+            <div className="footer">
+            <div >    
+            <ul className="resources">
+                <h2 className="RESOURCES">RESOURCES</h2>
+                {Resources.map((resources) => {
+                    return (
+                        <li className="resourcesItemLi" key={resources.id}>
+                            <Link className="resourcesItem"
+                                to={resources.link}>
+                                {resources.name}<br></br>
+                            </Link>
+                        </li>
+                    );
+                })}
+            
+                </ul>
+                </div>  
+                <div >
+                <ul className="support">
+                <h2 className="SUPPORT">SUPPORT</h2>
+                {Support.map((support) => {
+                    return (
+                        <li key={support.id} className="supportItemLi">
+                            <Link
+                                to={support.link} className="supportItem">
+                                {support.name}<br></br>
+                            </Link>
+                        </li>
+                    );
+                })}
+            
+                </ul>
+                </div>
+                <div >
+                <ul className="company">
+                <h2 className="COMPANY">COMPANY</h2>
+                {Company.map((company) => {
+                    return (
+                        <li key={company.id} className="companyItemLi">
+                            <Link
+                                to={company.link} className="companyItem">
+                                {company.name}<br></br>
+                            </Link>
+                        </li>
+                    );
+                })}
+            
+                </ul>
+                </div>
+                <div >
+                <ul className="legal">
+                <h2 className="LEGAL">LEGAL</h2>
+                {Legal.map((legal) => {
+                    return (
+                        <li key={legal.id} className="legalItemLi">
+                            <Link
+                                to={legal.link} className="legalItem">
+                                {legal.name}<br></br>
+                            </Link>
+                        </li>
+                    );
+                })}
+            
+                    </ul>
+                    </div>
+                <div className="input">
+                    <h3>GET YOUR API KEY</h3>
+                    <h4>Start a trial, get a free account for 30 days.</h4>
+                    <input className="email" placeholder="Enter your email" />
+                    <Link to="/go" className="email-button">Get Started</Link>
+                </div>
+                <div className="footer-last">
+                    <h4>
+                        © 2022 SaaS Village LLP. All rights reserved.
+                    </h4>
+                </div>
+               
+            </div>
+
+            
+        </> 
+    );
 }
 
-.resdiv {
-    
-}
-
-.resources {
-    color: #000000;
-    list-style-type: none;
-    font-size: 16px;
-    font-weight: 400;
-    line-height: 24px;
-    padding: 40px 60px;
-    width: 376px;
-    position: absolute;
-    left: 100px;
-    height: 30px;
-}
-
-
-.resourcesItemLi {
-    margin-top: 10px;
-    cursor: pointer;
-}
-
-.resourcesItem {
-    text-decoration: none;
-    color: #ffffff;
-    padding: 10px;
-    font-size: 16px;
-    font-weight: 400;
-    height: 18px;
-    line-height: 24px;
-    position: relative;
-    top: 30px;
-    margin-top: 60px;
-    width: 31px;
-}
-
-.RESOURCES {
-    color: #9ca3af;
-    font-size: 14px;
-    font-weight: 600;
-    height: 20px;
-    line-height: 20px;
-    margin: 0px;
-    padding: 0px;
-    width: 176px;
-    position: absolute;
-    left: 67px;
-}
-
-.supdiv {
-    
-}
-
-.support {
-    color: #000000;
-    list-style-type: none;
-    font-size: 16px;
-    font-weight: 400;
-    height: 180px;
-    line-height: 24px;
-    padding: 40px 60px;
-    width: 376px;
-    position: absolute;
-    left: 300px;
-}
-
-.supportItemLi {
-    margin-top: 10px;
-    cursor: pointer;
-}
-
-.supportItem {
-    text-decoration: none;
-    color: #ffffff;
-    font-size: 16px;
-    font-weight: 400;
-    height: 18px;
-    line-height: 24px;
-    position: relative;
-    top: 30px;
-    width: 331px;
-}
-
-.SUPPORT {
-    color: #9ca3af;
-    font-size: 14px;
-    font-weight: 600;
-    height: 20px;
-    line-height: 20px;
-    margin: 0px;
-    padding: 0px;
-    width: 176px;
-    position: absolute;
-    left: 60px;
-}
-
-.comdiv {
-    
-}
-
-.company {
-    color: #000000;
-    list-style-type: none;
-    font-size: 16px;
-    font-weight: 400;
-    height: 180px;
-    line-height: 24px;
-    padding: 40px 60px;
-    width: 376px;
-    position: absolute;
-    left: 450px;
-
-}
-
-.companyItemLi {
-    margin-top: 10px;
-    cursor: pointer;
-}
-
-.companyItem {
-    text-decoration: none;
-    color: #ffffff;
-    font-size: 16px;
-    font-weight: 400;
-    height: 18px;
-    line-height: 24px;
-    position: relative;
-    top: 30px;
-    width: 331px;
-}
-
-.COMPANY {
-    color: #9ca3af;
-    font-size: 14px;
-    font-weight: 600;
-    height: 20px;
-    line-height: 20px;
-    margin: 0px;
-    padding: 0px;
-    width: 176px;
-    position: absolute;
-    left: 60px;
-}
-
-.legaldiv {
-    position: absolute;
-}
-
-.legal {
-    color: #000000;
-    list-style-type: none;
-    font-size: 16px;
-    font-weight: 400;
-    height: 180px;
-    line-height: 24px;
-    padding: 40px 60px;
-    width: 376px;
-    position: relative;
-    left: 580px;
-
-
-}
-
-.legalItemLi {
-    margin-top: 10px;
-    cursor: pointer;
-}
-
-.legalItem {
-    text-decoration: none;
-    color: #ffffff;
-    font-size: 16px;
-    font-weight: 400;
-    height: 18px;
-    line-height: 24px;
-    position: relative;
-    top: 30px;
-    width: 331px;
-}
-
-.LEGAL {
-    color: #9ca3af;
-    font-size: 14px;
-    font-weight: 600;
-    height: 20px;
-    line-height: 20px;
-    margin: 0px;
-    padding: 0px;
-    width: 176px;
-    position: absolute;
-    left: 60px;
-}
-
-.input {
- position: absolute;
- top: 100px;
- right: 250px;
-
-}
-
-.email {
-    color: black;
-    font-size: 16px;
-    font-weight: 400;
-    height: 42px;
-    line-height: 24px;
-    margin-right: 10px;
-    padding: 8px 16px;
-    width: 252px;
-    background-color: #ffffff;
-    border-radius: 12px;
-}
-
-.email-button {
-    background-color: #4f46e5;
-    color: #ffffff;
-    text-decoration: none;
-    font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-    font-size: 16px;
-    font-weight: 500;
-    height: 92px;
-    line-height: 24px;
-    margin: 0px;
-    padding: 12px 16px;
-    width: 120px;
-    border-radius: 6px;
-}
-
-.input h3 {
-    color: #9ca3af;
-    font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-    font-size: 14px;
-    font-weight: 600;
-    height: 20px;
-    line-height: 20px;
-    margin: 0px;
-    padding: 0px;
-    width: 384px;
-}
-
-.input h4 {
-    color: #d1d5db;
-    font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-    font-size: 16px;
-    font-weight: 400;
-    height: 24px;
-
-    line-height: 24px;
-    width: 384px;
-
-}
-
-.footer-last {
-    color: #9ca3af;
-    font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-    font-size: 16px;
-    font-weight: 400;
-    height: 24px;
-    line-height: 24px;
-    position: relative;
-    left: 167px;
-    top: 0px;
-    padding: 0px;
-    width: 317px;
-
-}
+export default Footer;
